face8022fb3f23cb99002c23a34bdb18710c843e
shahidmehmood67 <shahidkotlin@gmail.com>
2026-02-18 17:13:08 +0500

    agp update 9.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -3,9 +3,11 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+//    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidMultiplatformLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
 }
 
 kotlin {
