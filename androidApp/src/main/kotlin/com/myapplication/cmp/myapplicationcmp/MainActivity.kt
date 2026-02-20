728aefc8c86d2b55ba038c6759e575836ef14332
shahidmehmood67 <shahidkotlin@gmail.com>
2026-02-20 11:39:07 +0500

    agp update 9.0 with warning resolved

--- androidApp/src/main/kotlin/com/myapplication/cmp/myapplicationcmp/MainActivity.kt
+++ androidApp/src/main/kotlin/com/myapplication/cmp/myapplicationcmp/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
-//agp update 9.0 libraries + new agp folder structure
+//agp update 9.0 libraries + new agp update with folder structure
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
